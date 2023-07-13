5ae010bf1448411daedbdb3bb4c98a9730e94ae2
BadrSayed <BadrSayed2@users.noreply.github.com>
2023-07-13 18:44:27 +0300

    created the compare guesses function that compares the selection of a player with the computer random guess

--- home.js
+++ home.js
@@ -2,12 +2,58 @@ function GetRandomRockPaperScissor(){
     let ComputerGuessProbability = Math.random()
     
     if(ComputerGuessProbability <=(1/3)){
-        return 'Rock'
+        return 'rock'
     } else if(ComputerGuessProbability <=(2/3) ){
-        return 'Paper'
+        return 'paper'
     } else {
-        return 'Scissor'
+        return 'scissors'
     }
 }
 
-console.log(GetRandomRockPaperScissor())
+function CompareGuesses( Guess , ComputerGuess){
+       Guess = Guess.toLowerCase();
+
+        if (Guess == ComputerGuess){
+            return 'It is a draw'
+        }
+
+        if(Guess == 'rock'){
+            
+            if (ComputerGuess == 'paper'){
+                return 'You lose Paper beats Rock'
+            } else {
+                return'You win Rock beats Scissors'
+            }
+
+        }
+
+        else if(Guess == 'paper'){
+            
+            if (ComputerGuess == 'scissors'){
+                return 'You lose Scissors beats Paper'
+            } else {
+                return'You win Paper beats Rock'
+            }
+        }
+
+        else if(Guess == 'scissors'){
+            
+            if (ComputerGuess == 'rock'){
+                return 'You lose Rock beats Scissors'
+            } else {
+                return'You win Scissors beats Paper'
+            }
+
+        }
+
+        else {
+            return 'Unknown answer , Please enter Rock or Paper or Scissors'
+        }
+
+}
+
+
+const playerSelection = 'Rock'
+const ComputerSelection = GetRandomRockPaperScissor()
+console.log(ComputerSelection)
+console.log(CompareGuesses(playerSelection , ComputerSelection))
